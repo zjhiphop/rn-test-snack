9547e6c001ca611ffc5ea5578721142fc41523b0
Jing Zhang <jing.zhang@oliverwyman.com>
2023-03-30 12:36:44 +0800

    snackfy

--- app/services/reactotron/index.ts
+++ app/services/reactotron/index.ts
@@ -1,1 +1,3 @@
-export * from "./reactotron"
+/*** This file is loaded for snack to resolve Reactotron for expo snack**/ 
+ export const setupReactotron = (arg1:any) => null 
+export const setReactotronRootStore=(arg1:any, arg2:any)=>null
